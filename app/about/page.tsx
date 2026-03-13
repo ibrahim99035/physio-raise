38fa77152ed8e0ab9ccee820268ec299fa54690e
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-03-13 16:48:38 +0200

    Fixing Deployment Issues

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,18 +1,4 @@
-'use client'
-
-import { FloatingParticles } from '@/components/layout'
-import { Navigation } from '@/components/navigation'
-import { Footer } from '@/components/footer'
-import { 
-  AboutHero, 
-  AboutDoctor, 
-  MissionVision, 
-  CoreValues, 
-  RecoveryProtocol,
-  Achievements,
-  AboutCTA 
-} from '@/components/about'
-import aboutData from '@/data/aboutDetails.json'
+import AboutPageClient from './AboutPageClient'
 import siteInfo from '@/data/siteInfo.json'
 import { Metadata } from 'next'
 
@@ -25,32 +11,5 @@ export const metadata: Metadata = {
 }
 
 export default function AboutPage() {
-  return (
-    <main className="min-h-screen">
-      <FloatingParticles />
-      <Navigation />
-      
-      <AboutHero 
-        title={aboutData.hero.title}
-        description={aboutData.hero.description}
-      />
-      
-      <AboutDoctor />
-      
-      <MissionVision 
-        mission={aboutData.mission}
-        vision={aboutData.vision}
-      />
-      
-      <CoreValues values={aboutData.values} />
-      
-      <RecoveryProtocol steps={aboutData.recoverySteps} />
-      
-      <Achievements achievements={aboutData.achievements} />
-      
-      <AboutCTA />
-      
-      <Footer />
-    </main>
-  )
+  return <AboutPageClient/>
 }
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,12 +1,5 @@
-'use client'
+import ServicesPageClient from './ServicesPageClient'
 
-import { FloatingParticles } from '@/components/layout'
-import { ServicesHero, ServicesGrid, ServicesCTA } from '@/components/services'
-
-import { Navigation } from '@/components/navigation'
-import { Footer } from '@/components/footer'
-
-import servicesData from '@/data/servicesDetails.json'
 import siteInfo from '@/data/siteInfo.json'
 import { Metadata } from 'next'
 
@@ -19,26 +12,5 @@ export const metadata: Metadata = {
 }
 
 export default function ServicesPage() {
-  return (
-    <main className="min-h-screen">
-      <FloatingParticles />
-      <Navigation />
-      
-      <ServicesHero 
-        title={servicesData.hero.title}
-        description={servicesData.hero.description}
-      />
-      
-      <ServicesGrid services={servicesData.services} />
-      
-      <ServicesCTA 
-        title={servicesData.cta.title}
-        titleAccent={servicesData.cta.titleAccent}
-        buttonText={servicesData.cta.buttonText}
-        buttonLink={servicesData.cta.buttonLink}
-      />
-      
-      <Footer />
-    </main>
-  )
+  return <ServicesPageClient />
 }
