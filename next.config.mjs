9dc1a6d599dc920bf8ba0772c33bc631c89d1469
Ibrahim abo eita <73365525+ibrahim99035@users.noreply.github.com>
2026-03-18 01:26:17 +0200

    Sitemap binary fix

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,23 @@ const nextConfig = {
     unoptimized: true,
     qualities: [75, 100],
   },
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml; charset=utf-8',
+          },
+          {
+            key: 'Content-Encoding',
+            value: 'identity',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
